762446cd7ad43cb8c6b4fd9b6adb9ac4df0d60a1
Samuel Lippert <lippertz@gmail.com>
2022-12-20 18:37:41 -0600

    Iterate

--- worker.js
+++ worker.js
@@ -15,33 +15,33 @@ export const api = {
   repo: 'https://github.com/drivly/encode.do',
 }
 
-export const gettingStarted = [
-  `If you don't already have a JSON Viewer Browser Extension, get that first:`,
-  `https://extensions.do`,
-]
+export const gettingStarted = [`If you don't already have a JSON Viewer Browser Extension, get that first:`, `https://extensions.do`]
 
 export const examples = {
-  "Encode String": 'https://encode.do/Hello, World!',
-  "Encode JSON": 'https://encode.do/?hello=world',
+  'Encode String': 'https://encode.do/Hello, World!',
+  'Encode JSON': 'https://encode.do/?hello=world',
 }
 
 export default {
   fetch: async (req, env) => {
-    const { user, hostname, pathname, rootPath, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
+    const { user, hostname, pathname, rootPath, pathSegments, query } = await env.CTX.fetch(req).then((res) => res.json())
     const hasQuery = Object.entries(query).length > 0
     const isUrl = pathSegments[0] === 'url'
-    if (!hasQuery && (rootPath || isUrl && pathSegments.length === 1)) return json({ api, gettingStarted, examples, user })
+    if (!hasQuery && (rootPath || (isUrl && pathSegments.length === 1))) return json({ api, gettingStarted, examples, user })
 
     let value = pathSegments[pathSegments.length - 1]
     if (isUrl) {
-      const buffer = await fetch(decodeURIComponent(value)).then(res => res.arrayBuffer())
-      value = btoa(String.fromCharCode(new Uint8Array(buffer)))
-    }
-    else if (hasQuery) value = JSON.stringify(query)
+      const buffer = await fetch(decodeURIComponent(value)).then((res) => res.arrayBuffer())
+      value = ''
+      new Uint8Array(buffer).forEach((byte) => {
+        value += String.fromCharCode(byte)
+      })
+      value = btoa(value)
+    } else if (hasQuery) value = JSON.stringify(query)
 
     const encoded = btoa(value)
     return json({ api, encoded, decoded: query ? query : value, user })
-  }
+  },
 }
 
-const json = obj => new Response(JSON.stringify(obj, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' } })
+const json = (obj) => new Response(JSON.stringify(obj, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' } })
